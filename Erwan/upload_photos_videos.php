3e72a5f9702ea5125ab0883d6697425091920d9c
Erwan Martin <contact@erwanmartin.fr>
2013-11-11 19:57:54 +0100

    Ajout d'une nouvelle fonctionnalité : les photos uploadées sont postées sur le compte Flickr et Twitter de l'application

--- Erwan/upload_photos_videos.php
+++ Erwan/upload_photos_videos.php
@@ -1,19 +1,64 @@
 <?php
+	session_start();
 	if (isset($_FILES["media"]) && $_FILES["media"]["error"] == 0){
 		$uploaddir = '/var/www/illio.fr/web/projet_html/tests_erwan/uploads/';
 		$uploadfile = $uploaddir . basename($_FILES['media']['name']);
 
-		echo '<pre>';
 		if (move_uploaded_file($_FILES['media']['tmp_name'], $uploadfile)) {
-		    echo "Le fichier est valide, et a été téléchargé
-		           avec succès. Voici plus d'informations :\n";
+			$_SESSION['upload_flickr'] = $uploadfile;
+			$_SESSION['upload_twitter'] = $uploadfile;
+			//$_SESSION['upload_twitter'] = 'http://www.find-id-out.fr/tests_erwan/uploads/'.basename($_FILES['media']['name']);
 		} else {
-		    echo "Attaque potentielle par téléchargement de fichiers.
-		          Voici plus d'informations :\n";
+		    echo('Une erreur s\'est produite. Veuillez recommencer.');
+		   	//print_r($_FILES);
+		}
+	}
+	if(isset($_SESSION['upload_flickr'])){
+		require_once('lib/flickr/phpFlickr.php');
+		$flickr = new phpFlickr(FLICKR_APP_KEY, FLICKR_APP_KEY_SECRET);
+		$flickr->setToken(FLICKR_USER_TOKEN);
+
+		if(empty($_GET['frob'])) {
+			$flickr->auth('write');
+
+			$result = $flickr->sync_upload($_SESSION['upload_flickr'], 'test', 'Test', 'test1, test2');
+			unset($_SESSION['upload_flickr']);
+			//var_dump($result);
+			echo('Votre photo a bien été sauvegardé ! <a href="http://www.flickr.com/photos/108169855@N05/" target="_blank">Flickr</a>');
+		}
+		else {
+			//$flickr->auth_getToken($_GET['frob']);
+			//header('Location: upload_photos_videos.php');
+			//exit();
+		}
+	}
+	if(isset($_SESSION['upload_twitter'])){
+
+		require_once('lib/tmhOAuth/tmhOAuth.php');
+
+		$connection = new tmhOAuth(array(
+			'consumer_key' => TWITTER_ACCESS_TOKEN,
+			'consumer_secret' => TWITTER_ACCESS_TOKEN_SECRET,
+			'user_token' => TWITTER_CONSUMER_KEY,
+			'user_secret' => TWITTER_CONSUMER_KEY_SECRET
+		));
+
+		$response_code = $connection->request( 'POST','https://api.twitter.com/1.1/statuses/update_with_media.json',
+	       array(
+	            'media[]'  => "@{$_SESSION['upload_twitter']};type=image/jpeg;filename={$_SESSION['upload_twitter']}",
+	            'status'   => 'test image',
+	       ),
+	        true,
+	        true
+	    );
+		 
+		if ($response_code == 200) {
+			echo('<br/>Tweet bien envoyé : <a href="https://twitter.com/Find_It_Out" target="_blank">Voir</a>');
+		} else {
+			echo('Erreur :'.$response_code.'');
 		}
 
-		echo 'Voici quelques informations de débogage :';
-		print_r($_FILES);
+		unset($_SESSION['upload_twitter']);
 	}
 
 ?>
@@ -26,14 +71,14 @@
 </head>
 <body>
 
-	<p>Test1</p>
-	<form enctype="multipart/form-data" action="upload_photos_videos.php" method="post">
+	<p>Photo</p>
+	<form enctype="multipart/form-data" action="upload_photos_videos_new.php" method="post">
 		<input type="file" name="media" capture="camera" accept="image/*" id="cameraInput">
 		<input type="submit" value="Upload">
 	</form>
 
 
-	<p>Test 2</p>
+	<p>Vidéo</p>
 	<form enctype="multipart/form-data" action="upload_photos_videos.php" method="post">
 	  <input type="file" name="media" accept="video/*" capture />
 	  <input type="submit" value="Upload">
