072bc054b52d969dde092014e598f6e21bbcf898
Erwan Martin <contact@erwanmartin.fr>
2013-11-13 17:21:36 +0100

    Remove header.inc.php

--- Erwan/ajax/get_lieu_by_id.xhr.php
+++ Erwan/ajax/get_lieu_by_id.xhr.php
@@ -5,8 +5,9 @@
 	$retour['infos'] = array();
 
 	if(isset($_POST['id_lieu'])){
-		define('CHEMIN_REQUIRES','../');
-		require_once(CHEMIN_REQUIRES.'includes/header.inc.php');
+		require_once('../includes/config.inc.php');
+		require_once('../classes/sql.class.php');
+		require_once('../includes/functions.inc.php');
 
 		$id = intval($_POST['id_lieu']);
 
--- Erwan/ajax/get_lieux_by_geolocalisation.xhr.php
+++ Erwan/ajax/get_lieux_by_geolocalisation.xhr.php
@@ -5,8 +5,9 @@
 	$retour['lieux'] = array();
 
 	if(isset($_POST['points'])){
-		define('CHEMIN_REQUIRES','../');
-		require_once(CHEMIN_REQUIRES.'includes/header.inc.php');
+		require_once('../includes/config.inc.php');
+		require_once('../classes/sql.class.php');
+		require_once('../includes/functions.inc.php');
 
 
 		$tab_lieux = explode(';',$_POST['points']);
