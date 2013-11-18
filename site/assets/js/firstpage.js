d3f96ce2e4d200db42e1f6504c14b45a7a702476
Erwan Martin <contact@erwanmartin.fr>
2013-11-18 22:02:39 +0100

    setting sidebar
    Signed-off-by: Nicolas Thomas <nicolas.thomas92@gmail.com>

--- site/assets/js/firstpage.js
+++ site/assets/js/firstpage.js
@@ -1,37 +1,40 @@
-function initialize() {
-  map = new google.maps.Map(document.getElementById("map-canvas"), {
-        zoom: 17,
-        center: new google.maps.LatLng(48.858565, 2.347198),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });  
-}
- 
-if (navigator.geolocation)
-  var watchId = navigator.geolocation.watchPosition(successCallback,
-                            null,
-                            {enableHighAccuracy:true});
-else
-  alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");   
- 
-function successCallback(position){
-  map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-    map: map
+$(function(){
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"), {
+    zoom: 17,
+    center: new google.maps.LatLng(48.858565, 2.347198),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false
   });
-}
 
+  if(typeof(stylesCarte)!='undefined'){
+    map.mapTypes.set('map_style', new google.maps.StyledMapType(stylesCarte));
+    map.setMapTypeId('map_style');
+  }
 
-$(document).ready(function() {
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        map: map,
+        icon: 'assets/img/maps_icons/icon_depart.svg'
+      });
+    });
+  }
 
-$('.bouton-connexion').click(function(e) {
-    $('#signup').lightbox_me({
-        centered: true, 
-        onLoad: function() { 
-            $('#signup').find('input:first').focus()
-            }
-        });
-    e.preventDefault();
-});
+  $('.bouton-connexion').click(function(e) {
+      $('#signup').lightbox_me({
+          centered: true, 
+          onLoad: function() { 
+              $('#signup').find('input:first').focus()
+              }
+          });
+      e.preventDefault();
+  });
 
 });
