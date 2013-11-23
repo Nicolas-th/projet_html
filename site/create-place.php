301111db71978d23f3ace649c388e08a8021753f
Ameziane Allioui <ameziane.allioui@gmail.com>
2013-11-23 16:24:11 +0100

    Modification url des css dans 'create-place.php'

--- site/create-place.php
+++ site/create-place.php
@@ -15,8 +15,8 @@
 		<meta name="description" content="">
 		
 		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="assets/css/place.css" />
-		<link rel="stylesheet" type="text/css" href="assets/css/simplegrid-lieu.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo($chemin_relatif_site); ?>assets/css/place.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo($chemin_relatif_site); ?>assets/css/simplegrid-lieu.css" />
 	</head>
 	<body>
 		<div id="container">
