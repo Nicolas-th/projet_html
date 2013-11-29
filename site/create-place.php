b2812deb56fa0f71bf3987af35b09b09b1380f0d
Erwan Martin <contact@erwanmartin.fr>
2013-11-29 01:16:25 +0100

    Finalisation de la page create-place.php avec geocodage en JS puis envoi en AJAX

--- site/create-place.php
+++ site/create-place.php
@@ -34,12 +34,12 @@
 	<?php if(!(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')) { ?>
 		<header>
 	  	<?php if(isset($_SESSION["id"])){ ?>
-			<a href="logout.php"><div class="icon"></div></a>
+			<a href="../logout.php"><div class="icon"></div></a>
 		<?php }else{ ?>
-			<a href="index.php" class="connexion">Connexion</a>
+			<a href="../index.php" class="connexion">Connexion</a>
 		<?php } ?>
 	  	</header>
-	  	<a href="index.php" id="bouton">Accueil</a> 
+	  	<a href="../index.php" id="bouton">Accueil</a> 
 	<?php } ?>
 		<div id="container">
 			<div class="grid grid-pad">
@@ -50,18 +50,33 @@
 
 			<div class="grid grid-pad">
 				<div class="col-1-1">
-					<form method="post" action="validate-place.php">
+					<form method="post" action="/ajax/validate-place.xhr.php" id="form-ajout-lieux">
 						<label for="namePlace">Nom du lieu*</label>
 						<input type="text" id="namePlace" name="namePlace" required/><br/><br/>
 						<label for="description" >Description*</label>
 						<textarea name="description" required></textarea><br/><br/>
 						
 						<div id="fields-select-submit">
+							
+							<div class="infos_localisation_lieu hide">
+								<label for="address" >Adresse*</label>
+								<input type="hidden" name="address" /><br/><br/>
+								<label for="postCode" >Code postal*</label>
+								<input type="hidden" name="postCode" /><br/><br/>
+								<label for="city" >Ville*</label>
+								<input type="hidden" name="city" />
+							</div>
+							<input type="hidden" name="latitude" />
+							<input type="hidden" name="longitude" />
+							<input type="hidden" name="valid" value="0" />
+
+							<label for="inOut" >Localisation</label>
 							<select name="inOut">
 								<option value="0">Intérieur</option>
 								<option value="1">Extérieur</option>
-							</select>
+							</select><br/><br/>
 
+							<label for="categorie" >Catégorie</label>
 							<select name="categorie">	
 								<?php
 								$sql = new SQL($dbh);
@@ -75,13 +90,6 @@
 								}
 								?>
 							</select>
-							
-							<input type="hidden" name="address" value="<?php echo $address; ?>" />
-							<input type="hidden" name="postCode" value="<?php echo $postCode; ?>" />
-							<input type="hidden" name="city" value="<?php echo $city; ?>" />
-							<input type="hidden" name="latitude" value="<?php echo $lat; ?>" />
-							<input type="hidden" name="longitude" value="<?php echo $long; ?>" />
-							<input type="hidden" name="valid" value="0" />
 						
 							<br><input class="submit" type="submit" name="valueButton" value="Soumettre" />
 						</div>
@@ -90,6 +98,10 @@
 			</div>
 
 		</div><!-- #container -->
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true&libraries=places&key=AIzaSyD2GbjbQbMiZrFHJN5b2L09ZenuQ8IzJUc&v=3.exp"></script>
+		<script type="text/javascript" src="js/localize.js"></script>
+		<script type="text/javascript" src="js/create-place.js"></script>
 		<script>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- site/js/localize.js
+++ site/js/localize.js
@@ -2,10 +2,19 @@ var localize={
 	
 	defaults : {
 		getUserLocation : {
-			localized : function(){}
+			localized : function(){},
+			error : function(){}
 		},
 		addTrack : {
 			added : function(){}
+		},
+		getAdress : {
+			success : function(){},
+			error : function(){}
+		},
+		getLocalization : {
+			success : function(){},
+			error : function(){}
 		}
 	},
 	
@@ -41,6 +50,54 @@ var localize={
 		if(navigator.geolocation){
 			navigator.geolocation.clearWatch(params.id);
 		}
+	},
+
+	getAdress : function(params){
+		params = $.extend(true, {},this.defaults.getUserLocation, params);
+		var geocoder = new google.maps.Geocoder();
+		var latlng = new google.maps.LatLng(params.latitude, params.longitude);
+		geocoder.geocode({
+			'latLng': latlng
+			},
+			function(data,status){
+				if(status == google.maps.GeocoderStatus.OK){
+					if(typeof(data[0])!='undefined' && typeof(data[0].address_components)!='undefined'){
+						var address_components = data[0].address_components;
+						params.success.call(this,{
+							adress : address_components[0].long_name+' '+address_components[1].long_name,
+							city : address_components[3].long_name,
+							postCode : address_components[7].long_name
+						});
+					}
+				}else{
+					params.error.call(this);
+				}
+			}
+		);
+	},
+
+	getLocalization : function(params){
+		params = $.extend(true, {},this.defaults.getUserLocation, params);
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+			'address': params.address
+			},
+			function(data,status){
+				if(status == google.maps.GeocoderStatus.OK){
+					if(typeof(data[0])!='undefined' && typeof(data[0].geometry)!='undefined'){
+						var position = data[0].geometry.location;
+						params.success.call(this,{
+							position : {
+								latitude : position.lat(),
+								longitude : position.lng()
+							}
+						});
+					}
+				}else{
+					params.error.call(this);
+				}
+			}
+		);
 	}
 	
 };
